4a1646fe47a7d0e46e15b9a375762cdacb1c1842
Austin Lindenberg <austin@appsumo.com>
2024-06-30 15:00:55 -0700

    debug

--- db/database.py
+++ db/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db").replace('postgres://', 'postgresql://')
 
+print("SQLALCHEMY DB URL ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
