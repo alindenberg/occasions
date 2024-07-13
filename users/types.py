142e9d033b9978597b936b0cebbb51dd519a0049
Austin Lindenberg <austin@appsumo.com>
2024-07-13 11:29:43 -0700

    serialize credits on users

--- users/types.py
+++ users/types.py
@@ -1,6 +1,9 @@
+import logging
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
-from typing import Optional
+from pydantic import BaseModel, EmailStr, field_serializer
+from typing import Optional, Any
+
+logger = logging.getLogger(__name__)
 
 
 class BaseUser(BaseModel):
@@ -11,7 +14,20 @@ class UserIn(BaseUser):
     password: str
 
 
+# class CreditOut(BaseModel):
+#     credits: int
+
+
 class UserOut(BaseUser):
     id: int
     created: datetime
     is_superuser: Optional[bool] = None
+    credits: Any
+
+    @field_serializer('credits')
+    def credits_serializer(self, credits, _info):
+        try:
+            return credits.credits
+        except Exception as exc:
+            logger.error(f"An error occurred while serializing credits: {exc}")
+            return 0
