ee782bcb2b8fb1dd4eceb095f02d34b6ae4406f7
Austin Lindenberg <austin@appsumo.com>
2025-03-11 21:43:05 -0700

    update email

--- mail/services.py
+++ mail/services.py
@@ -9,10 +9,10 @@ class MailService:
     def send_email(self, recipient_email, subject, body):
         settings = get_settings()
         requests.post(
-            "https://api.mailgun.net/v3/mg.occasionalert.me/messages",
+            "https://api.mailgun.net/v3/mg.occasionalerts.com/messages",
             auth=("api", settings.MAILGUN_API_KEY),
             data={
-                "from": "Occasion Alerts <mailgun@mg.occasionalert.me>",
+                "from": "Occasion Alerts <mailgun@mg.occasionalerts.com>",
                 "to": [recipient_email],
                 "subject": subject,
                 "text": body
